e48b7c1234ef2b284d3b9c067d26e45d59a1eeff
Giorgi Patashuri <55501704+giorgipatashuri@users.noreply.github.com>
2023-06-16 23:43:02 +0400

    Update axios.ts

--- core/axios.ts
+++ core/axios.ts
@@ -4,13 +4,11 @@ import { parseCookies } from 'nookies';
 // const url = process.env.API_URL || 'http://localhost:8888/';
 
 const url = process.env.NEST_API 
-console.log(url)
 axios.defaults.baseURL = url;
 
 axios.interceptors.request.use((config: any): any => {
   if (typeof window !== 'undefined') {
     const { _token } = parseCookies();
-console.log(url)
     
     config.headers.Authorization = 'Bearer ' + _token;
   }
