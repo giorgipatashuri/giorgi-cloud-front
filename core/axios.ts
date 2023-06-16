3a82a2f737c841cd469bc3c9d8ce69587fe0335f
Giorgi Patashuri <55501704+giorgipatashuri@users.noreply.github.com>
2023-06-16 16:06:34 +0400

    Update axios.ts

--- core/axios.ts
+++ core/axios.ts
@@ -1,8 +1,9 @@
 import axios, { InternalAxiosRequestConfig } from 'axios';
 import { parseCookies } from 'nookies';
 
-const url = process.env.API_URL || 'http://localhost:8888/';
+// const url = process.env.API_URL || 'http://localhost:8888/';
 
+const url = process.env.API_URL 
 axios.defaults.baseURL = url;
 
 axios.interceptors.request.use((config: any): any => {
